a7cebefcf13800550e08ea0b73806e5f35b046a5
hmm_kanmimimu <kanmimimudayo@proton.me>
2026-07-18 00:47:11 +0900

    init

--- atsa/src/main/java/jp/kaiz/atsassistmod/voicetext/Format.java
+++
@@ -1,6 +0,0 @@
-package jp.kaiz.atsassistmod.voicetext;
-
-/** 本家 jp.kaiz.atsassistmod.voicetext.Format の移植。 */
-public enum Format {
-    wav, ogg, mp3
-}
--- atsa/src/main/java/jp/kaiz/atsassistmod/voicetext/Speaker.java
+++
@@ -1,8 +0,0 @@
-package jp.kaiz.atsassistmod.voicetext;
-
-/** 本家 jp.kaiz.atsassistmod.voicetext.Speaker の移植。 */
-public enum Speaker {
-    show, haruka, hikari, takeru, santa, bear,
-    //実験的要素 (本家コメントアウトのまま)
-    //risa, erica, sayaka, akira, ryo
-}
